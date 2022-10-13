d6946cd2eaa6464dc6e78d7d299a4704218e7583
Robert <ojevsmack@gmail.com>
2022-10-13 14:31:24 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main function - entry point
+ * return 0 - exit point
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
