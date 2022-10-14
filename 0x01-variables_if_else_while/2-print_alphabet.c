4d1a1718f97a43833d2e41139ef6e08341622669
Robert <ojevsmack@gmail.com>
2022-10-14 13:54:37 -0700

    positive is better than negative nothing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lower case
+ *
+ * Return: 0 on completion
+ */
+
+int main(void)
+{
+	for (x = 'A', x < 'Z', x++)
+	{
+		x = tolower(x);
+
+		putchar(x);
+	}
+	return (0);
+}
