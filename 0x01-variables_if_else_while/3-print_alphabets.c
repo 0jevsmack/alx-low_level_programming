63ce1bc5b33ccf55e83e7a59fefa56c9d2950746
Robert <ojevsmack@gmail.com>
2022-10-14 14:35:56 -0700

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program prints alphabets in both cases
+ *
+ * Return: 0 to exit
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x < 'a'; x <= 'Z', x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
