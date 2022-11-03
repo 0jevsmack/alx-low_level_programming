b84adee7d2f2f5007f73928ec2c0afe7ad8ed487
Robert <ojevsmack@gmail.com>
2022-11-02 21:14:21 -0700

    added advanced tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
+
 /**
- * _puts_recursion - print a string
- * @s:string
- * Return:void
+ * _puts_recursion - prints a string and a new line
+ *
+ * @s: string to be printed
  */
+
 void _puts_recursion(char *s)
 {
-if (*s != 0)
-{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;	/* Always use return to avoid errror */
+	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
-}
-else
-{
-	_putchar('\n');
-}
-
 }
