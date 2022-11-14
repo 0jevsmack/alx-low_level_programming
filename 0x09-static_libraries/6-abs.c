f38bc82b4c3ac207af26644c7c5340741e4146b5
Robert <ojevsmack@gmail.com>
2022-11-14 05:47:13 -0800

    0-create_array.c added

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - comutes absolute value of an integer
+ * @n: value of int
+ * Return: void
+ *
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
