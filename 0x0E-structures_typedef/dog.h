81fc36173ef3af1b789244d43feab97af71a5843
Robert <ojevsmack@gmail.com>
2022-11-22 00:00:55 -0800

    dog struct created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog class
+ * @name: Dog's name
+ *
+ * @aage: Dog's age
+ *
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
